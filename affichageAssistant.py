58b93a072212c345791e3aec251a930c4c95d0bc
GabrielPlayer <teigne.gabriel@gmail.com>
2022-03-10 19:37:23 +0100

    Modification code application

--- affichageAssistant.py
+++ affichageAssistant.py
@@ -1,61 +1,67 @@
 from tkinter import *
-from time import *
+from time import localtime, strftime
 from tkinter import filedialog
 
-class Affichage:
-    def __init__(self,fenetre):
-        self.fenetre = fenetre
+class Application(Tk):
+    def __init__(self):        
+        Tk.__init__(self)
+
+        self.geometry("500x600")
+        self.title("Assistant")
+        self.resizable(width = False,height = False)
+
         self._texte = StringVar()
         self.actual_file = False
         self.listeTypes =[("fichier texte",".txt"),("autre fichier",".*")]
 
-    def application(self):
-        self.fenetre.geometry("500x600")
-        self.fenetre.title("Assistant")
-        self.fenetre.resizable(width = False,height = False)
+        self.creerWidgets()
+        self.creerMenu()
+        self.creerAction()
 
-        cadreDuBas = Frame(self.fenetre,bg = "#33A2FF")
-        cadreDuBas.pack(side = BOTTOM,fill = X)
-        self.cadreDuHaut = Frame(self.fenetre)
+        self.config(menu=self.mainmenu)
+
+    def creerWidgets(self):
+        self.cadreDuBas = Frame(self,bg = "#33A2FF")        
+        self.cadreDuBas.pack(side = BOTTOM,fill = X)
+        self.cadreDuHaut = Frame(self)
         self.cadreDuHaut.pack(side=TOP)
 
-        self.saisieDeTexte = Entry(cadreDuBas,textvariable = self._texte,width = 30,justify=CENTER,bd=1,takefocus=0)
+        self.saisieDeTexte = Entry(self.cadreDuBas,textvariable = self._texte,width = 30,justify=CENTER,bd=1,takefocus=0)
         self.saisieDeTexte.pack(side='left',padx = 85,pady = 25)
 
-        boutonVerif = Button(cadreDuBas,text="verifier",command = self.verifTexte)
-        boutonVerif.pack(padx = 5,pady = 25)
+        self.boutonVerif = Button(self.cadreDuBas,text="verifier",command = self.verifTexte)
+        self.boutonVerif.pack(padx = 5,pady = 25)
 
         self.texte = Text(self.cadreDuHaut,height=20,width=50,font="Calibri",maxundo = 14,undo=True,relief=FLAT)
         self.texte.configure(state='disabled')
-        self.texte.pack(pady = 20,padx = 5,side = LEFT)
+        self.texte.pack(pady = 20,padx = 5,side = LEFT)        
 
-        mainmenu = Menu(self.fenetre)
-        premierMenu = Menu(mainmenu,tearoff= 0)
-        deuxiemeMenu = Menu(mainmenu,tearoff= 0)
-        troisiemeMenu = Menu(mainmenu,tearoff= 0)
+        self.scrol = Scrollbar(self.cadreDuHaut,orient=VERTICAL,command = self.texte.yview)
+        self.scrol.pack(side = RIGHT,padx = 5,pady = 20,fill=Y)
 
-        premierMenu.add_command(label="Enregistrer",command = self.sauvegarder)
-        premierMenu.add_command(label="Enregistrer Sous",command = self.sauvegarder_sous)
+    def creerMenu(self):
+        self.mainmenu = Menu(self)
+        self.premierMenu = Menu(self.mainmenu,tearoff= 0)
+        self.deuxiemeMenu = Menu(self.mainmenu,tearoff= 0)
+        self.troisiemeMenu = Menu(self.mainmenu,tearoff= 0)
 
-        deuxiemeMenu.add_command(label="retour en arriere",command = self.texte.edit_undo)
-        deuxiemeMenu.add_command(label="retour en avant",command = self.texte.edit_redo)
+        self.premierMenu.add_command(label="Enregistrer",command = self.sauvegarder)
+        self.premierMenu.add_command(label="Enregistrer Sous",command = self.sauvegarder_sous)
 
-        troisiemeMenu.add_command(label="comment ça marche ?",command = self.info)
-        troisiemeMenu.add_command(label="Crédits",command = self.credits)
+        self.deuxiemeMenu.add_command(label="retour en arriere",command = self.texte.edit_undo)
+        self.deuxiemeMenu.add_command(label="retour en avant",command = self.texte.edit_redo)
 
-        mainmenu.add_cascade(label="Fichier",menu = premierMenu)
-        mainmenu.add_cascade(label="Edit",menu = deuxiemeMenu)
-        mainmenu.add_cascade(label="Info",menu = troisiemeMenu)
+        self.troisiemeMenu.add_command(label="comment ça marche ?",command = self.info)
+        self.troisiemeMenu.add_command(label="Crédits",command = self.credits)
 
-        scrol = Scrollbar(self.cadreDuHaut,orient=VERTICAL,command = self.texte.yview)
-        scrol.pack(side = RIGHT,padx = 5,pady = 20,fill=Y)
+        self.mainmenu.add_cascade(label="Fichier",menu = self.premierMenu)
+        self.mainmenu.add_cascade(label="Edit",menu = self.deuxiemeMenu)
+        self.mainmenu.add_cascade(label="Info",menu = self.troisiemeMenu)
 
+    def creerAction(self):
         self.texte.bind("<Control-s>", self.sauvegarder)
         self.saisieDeTexte.bind("<Control-s>", self.sauvegarder)
-        self.saisieDeTexte.bind("<Return>", self.verifTexte)
-
-        self.fenetre.config(menu=mainmenu)
-        self.fenetre.mainloop()
+        self.saisieDeTexte.bind("<Return>", self.verifTexte)        
 
     def verifTexte(self,*args):
         if self.saisieDeTexte.get() != "":
@@ -91,6 +97,5 @@ class Affichage:
         open(self.actual_file, "w").write(self.texte.get(0., END))
 
 if __name__ == "__main__":
-    fenetre = Tk()
-    fenetre = Affichage(fenetre)
-    fenetre.application()
+    app = Application()
+    app.mainloop()
